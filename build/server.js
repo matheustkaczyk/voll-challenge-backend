b1d485760a6194db2292c8c4a1da34dd766c36fd
matheustkaczyk <matheus111904@hotmail.com>
2022-05-05 13:32:06 -0300

    Configurando server para pegar PORT de variavel de ambiente, instalando dotenv

--- build/server.js
+++ build/server.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+app_1.app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
