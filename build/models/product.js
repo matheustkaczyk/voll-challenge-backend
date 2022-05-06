f909b94c88c7322a89370213b3f40caa63bd5a6c
matheustkaczyk <matheus111904@hotmail.com>
2022-05-06 14:47:08 -0300

    feat: criado metodo para procurar produto por id no bd

--- build/models/product.js
+++ build/models/product.js
@@ -12,9 +12,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createProduct = void 0;
+exports.findProductById = exports.createProduct = void 0;
 const product_1 = __importDefault(require("../database/schemas/product"));
 const createProduct = (product) => __awaiter(void 0, void 0, void 0, function* () {
     yield product_1.default.create(product);
 });
 exports.createProduct = createProduct;
+const findProductById = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const product = yield product_1.default.findById(id);
+    return product ? product.toObject() : null;
+});
+exports.findProductById = findProductById;
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,6 +1,11 @@
 import Product from "../database/schemas/product";
-import { Product as IProduct } from "../controllers/product/product.interface";
+import { Product as IProduct, ProductModel } from "../controllers/product/product.interface";
 
 export const createProduct = async (product: IProduct): Promise<void> => {
   await Product.create(product);
+}
+
+export const findProductById = async (id: string): Promise<ProductModel | null> => {
+  const product = await Product.findById(id);
+  return product ? product.toObject() : null;
 }
