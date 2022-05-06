61ae5095b23f16a7756d2d28656465e438373272
matheustkaczyk <matheus111904@hotmail.com>
2022-05-06 14:40:35 -0300

    feat: criando uma interface para referenciar o produto a ser cadastrado

--- src/controllers/product/product.interface.ts
+++ src/controllers/product/product.interface.ts
@@ -7,3 +7,11 @@ export interface ProductModel {
   stock: number;
   created_at: Date;
 }
+
+export interface Product {
+  name: string;
+  description: string;
+  img_url: string;
+  price: number;
+  stock: number;
+}
