31de0731002b618d1426718a7adbcf4ae3e5078d
matheustkaczyk <matheus111904@hotmail.com>
2022-05-10 11:03:30 -0300

    feat: configurando cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import userRoutes from './controllers/user/user-routes';
 import productRoutes from './controllers/product/product-routes';
 import saleRoutes from './controllers/sale/sale-routes';
-
+import cors from 'cors';
 export const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: "*"
+}))
 
 app.use('/user', userRoutes);
 app.use('/products', productRoutes);
