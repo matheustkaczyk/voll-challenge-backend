f594208b2ae5946c6377ec27697500e44444f5a2
matheustkaczyk <matheus111904@hotmail.com>
2022-05-10 12:41:12 -0300

    fix: tentando corrigir cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import userRoutes from './controllers/user/user-routes';
 import productRoutes from './controllers/product/product-routes';
 import saleRoutes from './controllers/sale/sale-routes';
 import cors from 'cors';
+
 export const app = express();
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
 app.use(express.json());
-app.use(cors({
-  origin: "*"
-}))
+app.use(cors(corsOptions));
 
 app.use('/user', userRoutes);
 app.use('/products', productRoutes);
